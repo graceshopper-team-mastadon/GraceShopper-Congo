7207cd572565aa11cf1e97d93868e0d87fe12a16
Andrew <andrewjacobalvarez22@gmail.com>
2023-02-28 10:00:28 -0600

    Boilder complete

--- client/App.js
+++ client/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
+import Home from './components/Home'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Login />} />
+        <Route exact path="/" element={<Home />} />
       </Routes>
     </>
   );
---
+++ client/components/Home.js
@@ -0,0 +1,7 @@
+const React = require("react");
+
+const Home = () => {
+  return  <h1>Welcome</h1>
+  
+};
+export default Home;
