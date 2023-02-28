8690b68e7a131af1cd2120af8539029669363f01
Andrew <andrewjacobalvarez22@gmail.com>
2023-02-28 16:43:44 -0600

    added future cookie and express login features

--- server/app.js
+++ server/app.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
 const Axios = require("axios");
 const path = require("path");
 const app = express();
+const loginRouter = require("./cookieRoutes/login");
 
 module.exports = app;
 
 app.use(express.json());
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use('/api', require('./api/api'));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/public/index.html"));
 });
+app.get("/login", loginRouter)
 
 app.use(express.static(path.join(__dirname, "..", "/public")));
 
