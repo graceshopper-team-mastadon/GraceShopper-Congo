ccfb7311ce65d46372a89bba09a1ca0798807d3c
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-01 11:47:14 -0600

    fixed bootstrap

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store/Store";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
