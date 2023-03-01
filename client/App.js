e3e179ef5874f8ae530657b5959ffd662e065a2c
Nica Weisinger <nica.weisinger@gmail.com>
2023-03-01 15:20:33 -0500

    Bootstrap formatting enabled

--- client/App.js
+++ client/App.js
@@ -3,10 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import AllProducts from "./components/BrowseProducts/AllProducts";
 import CategoryView from "./components/BrowseProducts/CategoryView";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <>
+      <div>
+        <nav>{<NavBar />}</nav>
+      </div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<AllProducts />} />
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
           presets: ["@babel/preset-react"],
         },
       },
+      {
+        test: /\.(sass|less|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 };
