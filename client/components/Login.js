a87c9205c12d65cb1486305a5f8aac38e32a397b
Alex Bernt <alexbernt100@gmail.com>
2023-03-01 15:14:32 -0600

    Center login and signup

--- client/components/Login.js
+++ client/components/Login.js
@@ -23,29 +23,34 @@ export default function Login() {
   //   }
   // }
   return (
-    <div className="w-100" style={{ maxWidth: "400px" }}>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
-          {/* {currentUser.email} */}
-          {/* {error && <Alert variant="danger">{error}</Alert>} */}
-          <Form>
-            <Form.Group id="username">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="username" required></Form.Control>
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" required></Form.Control>
-            </Form.Group>
-            <Button className="w-100" type="submit">
-              Log In
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Log In</h2>
+            {/* {currentUser.email} */}
+            {/* {error && <Alert variant="danger">{error}</Alert>} */}
+            <Form>
+              <Form.Group id="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" required></Form.Control>
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" required></Form.Control>
+              </Form.Group>
+              <Button className="w-100" type="submit">
+                Log In
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          Need an account? <Link to="/signup">Sign Up</Link>
+        </div>
       </div>
     </div>
   );
