0e88561b50be46eeb5f37e363dfea59f17f03616
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-02 15:45:08 -0600

    Ashley carry Exlamation Point

--- server/db/index.js
+++ server/db/index.js
@@ -4,12 +4,17 @@ const OrderProduct = require("./models/OrderProduct");
 const Product = require("./models/Product");
 const User = require("./models/User");
 
+// Old version 
+// OrderProduct.belongsTo(Product);
+// Product.hasMany(OrderProduct);
 
-OrderProduct.belongsTo(Product);
-Product.hasMany(OrderProduct);
+// OrderProduct.belongsTo(Order);
+// Order.hasMany(OrderProduct);
 
-OrderProduct.belongsTo(Order);
-Order.hasMany(OrderProduct);
+Product.belongsToMany(Order, {through: OrderProduct})
+Order.belongsToMany(Product, {through: OrderProduct})
+
+
 
 Order.belongsTo(User);
 User.hasMany(Order)
