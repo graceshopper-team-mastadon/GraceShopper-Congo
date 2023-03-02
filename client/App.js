c5a7ffa7110c00a7281d2253fbbe01206e706bbc
Nica Weisinger <nica.weisinger@gmail.com>
2023-03-02 10:46:01 -0500

    NavBar functionality

--- client/App.js
+++ client/App.js
@@ -4,15 +4,15 @@ import Home from "./components/Home";
 import AllProducts from "./components/BrowseProducts/AllProducts";
 import CategoryView from "./components/BrowseProducts/CategoryView";
 import NavBar from "./components/NavBar";
+import PokeHome from "./components/PokeHome";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 
 const App = () => {
   return (
     <>
-      <div>
-        <nav>{<NavBar />}</nav>
-      </div>
+      <div>{<PokeHome />}</div>
+      <div>{<NavBar />}</div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<AllProducts />} />
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,7 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
-  return <nav className="navbar navbar-light bg-primary">NavBar</nav>;
+  return (
+    <div>
+      <Navbar className="navbar bg-light">
+        <Container>
+          <Nav.Link href="">Favorites</Nav.Link>
+          <NavDropdown title="Pokemon" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/products">All Pokemon</NavDropdown.Item>
+            <NavDropdown.Item href="">Types</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="">Pokeballs</Nav.Link>
+          <Nav.Link href="">Potions</Nav.Link>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 export default NavBar;
