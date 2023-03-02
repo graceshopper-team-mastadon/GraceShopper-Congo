5ced628f2a7212ec322f0fbdd89988b5c4e6a0dd
Michelle <mzou1223@gmail.com>
2023-03-02 16:31:48 -0500

    added a bunch of components

---
+++ client/components/AccountView/OrderHistory.js
@@ -0,0 +1,13 @@
+const React = require("react");
+
+const OrderHistory = () => {
+    return (
+        <>
+            <div>
+                <h1> this is orderhistory lol </h1>
+            </div>
+        </>
+    )
+
+};
+export default OrderHistory;
--- client/components/PokeHome.js
+++ client/components/PokeHome.js
@@ -16,23 +16,19 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const PokeHome = () => {
   const [show, setShow] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  let navigate = useNavigate();
 
   useEffect(() => {
     const thing = async () => {
-      const { data } = await axios.get('./auth/verify');
-      console.log('data', data)
-      setLoggedIn(data);
+      const data = await axios.get('./auth/verify');
+      setLoggedIn(data)
     };
     thing();
-    navigate('/')
   }, [])
 
   return (
@@ -46,27 +42,26 @@ const PokeHome = () => {
             </FormGroup>
             <Button type="submit">Submit</Button>
           </Form>
-
           <Link to="/cart">Cart</Link>
-          {!loggedIn ?
-            <> <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link> </> : <>
-              <Nav.Link href="" onClick={handleShow}>Account</Nav.Link>
-              <Offcanvas show={show} onHide={handleClose} placement="end">
-                <Offcanvas.Header closeButton>
-                  <Offcanvas.Title>Account</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                  <ListGroup>
-                    <ListGroup.Item><Nav.Link href="/profile">Profile </Nav.Link></ListGroup.Item>
-                    <ListGroup.Item>Order History</ListGroup.Item>
-                    <ListGroup.Item>Address Book</ListGroup.Item>
-                    <ListGroup.Item>Payment Cards</ListGroup.Item>
-                    <ListGroup.Item>Send us Feedback</ListGroup.Item>
-                  </ListGroup>
-                </Offcanvas.Body>
-              </Offcanvas></>}
 
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Nav.Link href="" onClick={handleShow}>Account</Nav.Link>
+          <Offcanvas show={show} onHide={handleClose} placement="end">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Account</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <ListGroup>
+                <ListGroup.Item><Nav.Link href="/profile">Profile </Nav.Link></ListGroup.Item>
+                <ListGroup.Item><Nav.Link href="/order-history">Order History </Nav.Link></ListGroup.Item>
+                <ListGroup.Item><Nav.Link href="/address-book">Address Book </Nav.Link></ListGroup.Item>
+                <ListGroup.Item><Nav.Link href="/payment-cards">Payment Cards </Nav.Link></ListGroup.Item>
+                <ListGroup.Item><Nav.Link href="/feedback">Send us Feedback </Nav.Link></ListGroup.Item>
+                <ListGroup.Item>Sign Out</ListGroup.Item>
+              </ListGroup>
+            </Offcanvas.Body>
+          </Offcanvas>
         </Container>
       </Navbar>
     </div>
