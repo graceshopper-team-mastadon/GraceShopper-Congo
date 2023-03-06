4967e9d5750684826baf9beb3b5557a375158135
Michelle <mzou1223@gmail.com>
2023-03-06 18:01:31 -0500

    funny thing

--- client/components/BrowseProducts/Product.js
+++ client/components/BrowseProducts/Product.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 import QuickAdd from "./QuickAdd";
 
 const Product = (props) => {
   const { product } = props;
 
   return (
-    <div className="product-element">
-      <h2>
-        <Link to={`/products/${product.id}`}>{product.name}</Link>
-      </h2>
-      <img src={product.imageUrl} />
-      <div>{product.price}</div>
-      <div>{<QuickAdd product={product} key={product.id} />}</div>
+    <div className="d-flex justify-content-around">
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={product.imageUrl} />
+        <Card.Body>
+          <Card.Title><Link to={`/products/${product.id}`}>{product.name}</Link></Card.Title>
+          <Card.Text>
+            Price: {`${product.price}`} <br></br>
+            Why is he so squished?</Card.Text>
+          {<QuickAdd product={product} key={product.id} />}
+        </Card.Body>
+      </Card>
     </div>
   );
 };
 
 export default Product;
+
