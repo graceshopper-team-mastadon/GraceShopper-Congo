df27b8522801eab05f279fe9ce6d01067e9936fe
Nica Weisinger <nica.weisinger@gmail.com>
2023-03-07 17:41:44 -0500

    Links fixed

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -6,15 +6,29 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const linkStyle = {
+    margin: "1rem",
+    textDecoration: "none",
+    fontFamily: "optima",
+    color: "steelblue",
+  };
+
   return (
     <div>
       <Navbar className="navbar bg-light">
         <Container>
-          <Link to="/products">All Products</Link>
-          <Link to="/new">New Selection </Link>
+          <Link to="/products" style={linkStyle}>
+            All Products
+          </Link>
+          <Link to="/new" style={linkStyle}>
+            New Selection{" "}
+          </Link>
           <NavDropdown title="Pokemon" id="basic-nav-dropdown">
             <NavDropdown.Item>
-              <Link to="/category/pokemon-all"> All Pokemon</Link>
+              <Link to="/category/pokemon-all" style={linkStyle}>
+                {" "}
+                All Pokemon
+              </Link>
             </NavDropdown.Item>
             <NavDropdown.Item href="">Generation I</NavDropdown.Item>
             <NavDropdown.Item href="">Generation II</NavDropdown.Item>
@@ -22,8 +36,12 @@ const NavBar = () => {
             <NavDropdown.Item href="">Generation IV</NavDropdown.Item>
             <NavDropdown.Item href="">Generation V</NavDropdown.Item>
           </NavDropdown>
-          <Link to="/category/pokeballs">Pokeballs</Link>
-          <Link to="/category/potions">Potions</Link>
+          <Link to="/category/pokeballs" style={linkStyle}>
+            Pokeballs
+          </Link>
+          <Link to="/category/potions" style={linkStyle}>
+            Potions
+          </Link>
         </Container>
       </Navbar>
     </div>
