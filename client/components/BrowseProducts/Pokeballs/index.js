31a0d7f3732589fc552c75d130e37f6c384dc18c
Nica Weisinger <nica.weisinger@gmail.com>
2023-07-01 22:50:57 -0400

    Pokeballs pagination formatted

--- client/components/BrowseProducts/Pokeballs/index.js
+++ client/components/BrowseProducts/Pokeballs/index.js
@@ -6,41 +6,39 @@ import Pagination from "../../Pagination";
 import { useState } from "react";
 
 const Pokeballs = () => {
-    const dispatch = useDispatch();
-    const allpokeballs = useSelector((state) => state.categories.pokeballs);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(5);
+  const dispatch = useDispatch();
+  const allpokeballs = useSelector((state) => state.categories.pokeballs);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
 
-    useEffect(() => {
-        dispatch(getAllPokeballs());
-    }, []);
+  useEffect(() => {
+    dispatch(getAllPokeballs());
+  }, []);
 
-    const indexOfLastItems = currentPage * itemsPerPage;
-    const indexOfFirstItems = indexOfLastItems - itemsPerPage;
-    const currentItems = allpokeballs.slice(indexOfFirstItems, indexOfLastItems);
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const indexOfLastItems = currentPage * itemsPerPage;
+  const indexOfFirstItems = indexOfLastItems - itemsPerPage;
+  const currentItems = allpokeballs.slice(indexOfFirstItems, indexOfLastItems);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    if (allpokeballs.length === 0) {
-        return <h1> Loading your products!! </h1>
-    } else {
-        return (
-            <>
-                <div class="deck">
-                    {allpokeballs.map((pokeball) => (
-                        <PokeballElement key={pokeball.id} pokeball={pokeball} />
-                    )
-                    )}
-                </div>
-                <div>
-                    <Pagination
-                        itemsPerPage={itemsPerPage}
-                        totalItems={allpokeballs.length}
-                        paginate={paginate}
-                    />
-                </div>
-            </>
-        )
-    }
-
+  if (allpokeballs.length === 0) {
+    return <h1> Loading your products!! </h1>;
+  } else {
+    return (
+      <div className="browse">
+        <div class="deck">
+          {allpokeballs.map((pokeball) => (
+            <PokeballElement key={pokeball.id} pokeball={pokeball} />
+          ))}
+        </div>
+        <div className="paginate">
+          <Pagination
+            itemsPerPage={itemsPerPage}
+            totalItems={allpokeballs.length}
+            paginate={paginate}
+          />
+        </div>
+      </div>
+    );
+  }
 };
 export default Pokeballs;
