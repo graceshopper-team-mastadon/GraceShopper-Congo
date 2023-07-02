68e9729e244703f55ddc172999ebcf9c93eca01d
Nica Weisinger <nica.weisinger@gmail.com>
2023-07-01 22:52:00 -0400

    Pokemon view pagination formatted

--- client/components/BrowseProducts/Pokemon/index.js
+++ client/components/BrowseProducts/Pokemon/index.js
@@ -6,41 +6,39 @@ import Pagination from "../../Pagination";
 import { useState } from "react";
 
 const Pokemon = () => {
-    const dispatch = useDispatch();
-    const pokemons = useSelector((state) => state.categories.pokemon);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(5);
+  const dispatch = useDispatch();
+  const pokemons = useSelector((state) => state.categories.pokemon);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
 
-    useEffect(() => {
-        dispatch(getAllPokemon());
-    }, []);
+  useEffect(() => {
+    dispatch(getAllPokemon());
+  }, []);
 
-    const indexOfLastItems = currentPage * itemsPerPage;
-    const indexOfFirstItems = indexOfLastItems - itemsPerPage;
-    const currentItems = pokemons.slice(indexOfFirstItems, indexOfLastItems);
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const indexOfLastItems = currentPage * itemsPerPage;
+  const indexOfFirstItems = indexOfLastItems - itemsPerPage;
+  const currentItems = pokemons.slice(indexOfFirstItems, indexOfLastItems);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    if (pokemons.length === 0) {
-        return <h1> Loading your products!! </h1>
-    } else {
-        return (
-            <>
-                <div class="deck">
-                    {currentItems.map((pokemon) => (
-                        <PokemonElement key={pokemon.id} pokemon={pokemon} />
-                    )
-                    )}
-                </div> <br></br>
-                <Pagination
-                    itemsPerPage={itemsPerPage}
-                    totalItems={pokemons.length}
-                    paginate={paginate}
-                />
-            </>
-        )
-    }
-
+  if (pokemons.length === 0) {
+    return <h1> Loading your products!! </h1>;
+  } else {
+    return (
+      <div className="browse">
+        <div class="deck">
+          {currentItems.map((pokemon) => (
+            <PokemonElement key={pokemon.id} pokemon={pokemon} />
+          ))}
+        </div>
+        <div className="paginate">
+          <Pagination
+            itemsPerPage={itemsPerPage}
+            totalItems={pokemons.length}
+            paginate={paginate}
+          />
+        </div>
+      </div>
+    );
+  }
 };
 export default Pokemon;
-
-
